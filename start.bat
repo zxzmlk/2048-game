c3b3663e9f76df09d27eb7087dcb4a1699b3eba6
belovnv <blovnv@gmail.com>
2025-02-18 17:35:48 +0300

    Правки

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
-docker build -t 2048-game .
+docker build -t 2048-game:v1 .
 
-docker run -d --name game-container-1 -p 8081:8080 2048-game
+docker run -d --name game-1 -p 8081:8080 2048-game:v1
 
-docker run -d --name game-container-2 -p 8082:8080 2048-game
+docker run -d --name game-2 -p 8082:8080 2048-game:v1
